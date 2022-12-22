ca5768b2e90dd8e4ef9abd34d51e09f94e0101b7
Adam Beigel <31280510+abeige@users.noreply.github.com>
2022-12-22 10:35:01 -0600

    feat: simulate images
    feat: upload PictureCreation folder and rename
    
    defined a bunch of variables and made the script for circles
    
    target generator is done
    
    I pulled an all-nighter for this... just to make fucking pictures
    
    leftover
    
    fixed a couple of things
    
    new master images* (the files are too big)
    
    the thing is that we are dealing with a 1GB photo and most likely will take a while to generate all the background images using flip image augmentation
    
    Im going insane
    
    for some reason whenever I try to use opencv's bounding box it always throws this error: cv2.error: OpenCV(4.6.0) D:\a\opencv-python\opencv-python\opencv\modules\imgproc\src\convhull.cpp:143: error: (-215:Assertion failed) total >= 0 && (depth == CV_32F || depth == CV_32S) in function 'cv::convexHull'
    
    IDK what this means and its 2 am and I just want this to work
    
    final a some what working script
    
    whats left: figure out how to get a more accurate bounding box, how to crop a rotated rectangle
    
    updated ignores
    
    finally creates the images with all features
    
    but the image of the picture is bad quality due to all the rotating so im going to rewrite it. Also, the coordinates for the bounding box is not right for some reason.
    
    alright the valid camera image generation
    
    basically fixed the transformations
    
    end of me testing stuff
    
    All I have done testenv has like what the script really should be.
    
    changed some coords
    
    ayo no rotation it works so far
    
    It works. It accurately represents the runway
    
    finally finished ( to use import pain and run fnl_valid_gen)
    
    cleaning the branch up and switched to jpgs bc smaller
    
    added a test
    
    initial exploration and math
    
    i am god
    
    check if target in snapshot
    
    data saving done
    
    change imports and use of var
    
    curse you advik the platypus!!!!!!!!!
    
    I rewrote the whole thing again
    
    fix some file logic
    
    ran some tests and entered stats
    
    word and decimal points
    
    remove notebook

---
+++ simulate-images/target.py
@@ -0,0 +1,113 @@
+import random
+
+from PIL import Image, ImageDraw, ImageFont
+from shapely import affinity
+from shapely.geometry import box
+
+import vars
+
+# constants
+colors = {
+    "white": (255, 255, 255),  # White
+    "black": (0, 0, 0),  # Black
+    "gray": (127, 127, 127),  # Gray
+    "red": (255, 0, 0),  # Red
+    "green": (0, 255, 0),  # Green
+    "blue": (0, 0, 255),  # Blue
+    "yellow": (255, 255, 0),  # Yellow
+    "purple": (127, 0, 255),  # Purple
+    "orange": (255, 127, 0),  # Orange
+    "brown": (72, 59, 39),  # Brown
+}
+
+shapes = (
+    "triangle", "square", "pentagon", "hexagon", "heptagon",
+    "octagon", "circle", "semicircle", "quartercircle",
+    "rect", "star", "trap", "cross"
+)
+
+special_cases = ["semicircle", "quartercircle",
+                 "rect", "star", "trap", "cross"]
+polygons = ["triangle", "square", "pentagon", "hexagon", "heptagon", "octagon"]
+
+
+# create a rotated target image and polygon
+def createTarget():
+    # choose colors for shape and letter
+    randColors = random.sample(list(colors.keys()), k=2)
+    seed = {
+        "shape": shapes[random.randint(0, 12)],
+        "shapeColor": randColors[0],
+        "letter": chr(random.randint(65, 90)),
+        "letterColor": randColors[1],
+    }
+
+    # open a new image
+    img = Image.new(mode="RGBA", size=vars.targetSize, color="white")
+    img_draw = ImageDraw.Draw(img)
+    img_font = ImageFont.truetype(
+        vars.resourceDir + "arial.ttf", int(vars.targetSize[0] / 2))
+
+    # draw the shape
+    if (seed["shape"] == "circle"):
+        img_draw.ellipse([0, 0, img.size],
+                         fill=colors[seed["shapeColor"]], width=0)
+    elif (seed["shape"] in special_cases):
+        with Image.open(vars.resourceDir + seed["shape"] + ".bmp") as bitFile:
+            scaleFactor = vars.targetSize[0] / bitFile.width
+            newSize = [int(bitFile.width * scaleFactor),
+                       int(bitFile.height * scaleFactor)]
+            scaledBitFile = bitFile.resize(newSize)
+            img_draw.bitmap([0, 0], scaledBitFile,
+                            fill=colors[seed["shapeColor"]])
+    else:
+        img_draw.regular_polygon(
+            [img.size[0] / 2, img.size[1] / 2, img.size[0] / 2],
+            polygons.index(seed["shape"]) + 3,
+            fill=seed["shapeColor"]
+        )
+
+    # draw the letter
+    img_draw.text(
+        [vars.targetSize[0] / 2, vars.targetSize[1] / 2],
+        seed["letter"],
+        fill=colors[seed["letterColor"]],
+        anchor="mm",
+        font=img_font
+    )
+
+    # create a polygon for the target
+    polygon = box(0, 0, img.size[0], img.size[1])
+
+    # rotate the target
+    targetRotation = random.randint(0, 359)
+    img = img.rotate(targetRotation, expand=True, fillcolor="#0000")
+
+    # rotate the polygon
+    # negative for different origin
+    polygon = affinity.rotate(polygon, -targetRotation)
+    polygon = affinity.translate(
+        polygon,
+        xoff=-polygon.bounds[0],
+        yoff=-polygon.bounds[1]
+    )  # snap to axes
+
+    return (img, polygon)
+
+
+# move the target polygon to a random location within the snapshot
+def moveTarget(polygon):
+    xMin, yMin, xMax, yMax = [int(b) for b in polygon.bounds]
+    targetWidth = xMax - xMin
+    targetHeight = yMax - yMin
+
+    # choose random location
+    offset = [
+        random.randint(0, vars.imageSizePx[0] - targetWidth),
+        random.randint(0, vars.imageSizePx[1] - targetHeight),
+    ]  # upper left corner of target
+
+    # move to location
+    polygon = affinity.translate(polygon, xoff=offset[0], yoff=offset[1])
+
+    return polygon
---
+++ simulate-images/vars.py
@@ -0,0 +1,33 @@
+from math import sqrt, atan, tan
+
+from shapely.geometry.polygon import Polygon
+from shapely.geometry import box
+
+# directories
+noTargetDir = "snapshots/no-target/"
+targetDir = "snapshots/target/"
+resourceDir = "target-resources/"
+
+# constants
+sensorSize = 7.9  # mm
+focalLength = 16  # mm
+droneHeight = 100  # ft
+imageSizePx = (4056, 3040)  # px
+targetSizeFt = (8.5 / 12, 11 / 12)  # ft (8.5x11" page)
+satelliteSizePx = (4950, 1650)  # px
+runwayPxPerFt = 4150 / 360  # px/ft
+
+# constant shapes
+airDropBoundary = box(400, 400, 4550, 1250)
+
+# calculated values
+imageDiagPx = sqrt(imageSizePx[0] ** 2 + imageSizePx[1] ** 2)  # px
+viewAngle = 2 * atan(sensorSize / 2 / focalLength)  # rad
+imageDiagFt = 2 * droneHeight * tan(viewAngle / 2)  # ft
+
+# final values
+pxPerFt = imageDiagPx / imageDiagFt  # px/ft
+imageSizeFt = [l / pxPerFt for l in imageSizePx]  # ft
+scaleFactor = pxPerFt / runwayPxPerFt  # no unit
+(snapshotWidth, snapshotHeight) = [int(dim * runwayPxPerFt) for dim in imageSizeFt]  # px
+targetSize = [int(f * pxPerFt) for f in targetSizeFt]  # px
