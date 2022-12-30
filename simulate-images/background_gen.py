c42b945d12eaa1492c6c309ca6190a142dc5ba47
Asik007 <yugioh705@gmail.com>
2022-12-29 23:07:23 -0600

    finally creates the images with all features
    but the image of the picture is bad quality due to all the rotating so im going to rewrite it. Also, the coordinates for the bounding box is not right for some reason.

--- simulate-images/background_gen.py
+++ simulate-images/background_gen.py
@@ -1,7 +1,9 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTransform
 import random
+import math
 import cv2
 import numpy as np
+import vars
 from shapely.geometry import Polygon, MultiPoint, Point
 import os
 
@@ -17,15 +19,17 @@ def polygon_random_points (poly):
         if (random_point.within(poly)):
             points.append(random_point)
     return points[0]
+
 def generate_final(amount):
     for _ in range(amount):
+        print("Which image it is on rn:",_)
         tl_point = polygon_random_points(polygon)
 
         print(tl_point)
 
         point = (int(tl_point.x),int(tl_point.y))
 
-        seed = [random.randint(0,359), point[0], point[1]]
+        seed = [random.randint(0,89), point[0], point[1]]
 
         tar_files = os.listdir("target_images")
 
@@ -40,32 +44,84 @@ def generate_final(amount):
         # print(bgd_img.mode)
 
         rot_img = tar_img.rotate(seed[0],expand=True, fillcolor="#00000000")
+        theta_rad = math.radians(seed[0])
+        bbox = [0, 0, 0, 0]
+        bbox[0] = [0,int(math.sin(theta_rad) * vars.tar_res[0])]
+        bbox[1] = [int(math.cos(theta_rad) * vars.tar_res[0]),0]
+        bbox[2] = [rot_img.width-1,int(math.cos(theta_rad) * vars.tar_res[1])]
+        bbox[3] = [int(math.sin(theta_rad) * vars.tar_res[1]),rot_img.height-1]
+        print(bbox)
         # rot_img.show()
         # print(rot_img.mode)
         angle = seed[0]
         bgd_img.alpha_composite(rot_img,dest=point)
+        for arr in bbox:
+            arr[0] += point[0]
+            arr[1] += point[1]
+            print(arr)
 
         # bgd_img.crop()
-
+        img = ImageDraw.Draw(bgd_img)
+        for arr in bbox:
+            img.point(arr,fill="Green")
+            # print("point done")
         # bgd_img.show()
 
+        print(bbox)
+        print("master{}__{}__{}".format(seed[0],seed[1],seed[2])+".jpg")
         bgd_img.convert("RGB").save(master_dir + "master{}__{}__{}".format(seed[0],seed[1],seed[2])+".jpg")
-        tlx_crop = random.randint(0,2864)
-        tly_crop = random.randint(0,2688)
-        while not(tlx_crop < seed[1] < tlx_crop+4032 and tlx_crop < seed[1]+rot_img.width < tlx_crop+4032):
-            tlx_crop = random.randint(0, 2864)
-            print("retry")
-        while not(tly_crop < seed[2] < tly_crop+3040 and tly_crop < seed[2]+rot_img.height < tly_crop+3040):
-            print("retry")
-            tly_crop = random.randint(0, 2688)
-        print(str(tlx_crop)+" "+str(tly_crop)+" "+str(tlx_crop+4032)+" "+str(tly_crop+3040))
-        print(bgd_img.size)
-        brx_crop = tlx_crop+4032
-        bry_crop = tly_crop+3040
-        fnl_img = bgd_img.crop((tlx_crop,tly_crop,brx_crop,bry_crop))
-        fnl_img.convert("RGB").save(final_dir+"{}_{}_{}_{}.jpg".format(seed[0],seed[1]-tlx_crop,seed[2]-tly_crop,tar_path))
+
+        valid = False
+        target = Polygon(bbox)
+        fnl_box = []
+        corner = []
+        rot_arr = []
+        while valid == False:
+            rot = random.randint(0, 359)
+            rot_tht = math.radians(rot)
+            corner = [random.randint(0, bgd_img.width-1), random.randint(0, bgd_img.height-1)]
+            # print(corner)
+            box = [[0, 0], [4032, 0], [4032, 3040], [0, 3040]]
+            rot_arr = [[math.cos(rot_tht), -1 * math.sin(rot_tht)], [math.sin(rot_tht), math.cos(rot_tht)]]
+            rot_box = np.dot(box, rot_arr)
+            fnl_box = []
+            for i in range(len(box)):
+                coord = np.add(rot_box[i], corner)
+                # print(coord[0])
+                # print(coord[1])
+                if 0 < coord[0] < bgd_img.width-1 and 0 < coord[1] < bgd_img.height-1:
+                    valid = True
+                    # print(valid)
+                else:
+                    valid = False
+                    break
+                fnl_box.append(list(coord))
+            if len(fnl_box) == 4:
+                cam = Polygon(fnl_box)
+                if not (cam.contains(target)):
+                    valid = False
+                    # print("welp")
+                else:
+                    print("pog")
+            # print(valid)
+            # print(fnl_box)
+        print(corner)
+        for i in range(len(bbox)):
+            bbox[i] = np.subtract(bbox[i], corner)
+            print(bbox[i])
+        print(rot_arr)
+        bbox = np.dot(bbox, rot_arr)
+
+        print(bbox)
+
+        fnl_box = np.ravel(fnl_box)
+        fnl_img = bgd_img.transform((4032,3040), ImageTransform.QuadTransform(fnl_box))
+        print("{}_{}_{}_{}.jpg".format(seed[0],seed[1],seed[2],tar_path))
+        # fnl_img.show()
+        fnl_img.convert("RGB").save(final_dir+"{}_{}_{}_{}.jpg".format(seed[0],seed[1],seed[2],tar_path))
     return
 
+generate_final(1)
 #BIG NOTE
 # Someone who knows how to do linear algebra figure out a way for us to use the amount we
 # rotate each target or seed[0] and the fact that the targets are a given size
--- simulate-images/target_gen.py
+++ simulate-images/target_gen.py
@@ -39,15 +39,15 @@ tar_colors_names=[
 def gen_targets(amount):
     for x in range(amount):
         rand_color = random.sample(range(9), k=2)
-        seed = [random.randint(0,12),rand_color[0],chr(random.randint(65,90)), 90*random.randint(0,3), rand_color[1]]
+        seed = [random.randint(0,12),rand_color[0],chr(random.randint(65,90)), rand_color[1]]
         # seed = [6, 4, chr(65), 0]
         #first is selecting a random shape from tar_shape, second is random from colors, thrid is the letter, 4 is rotation
-        print("seed: {} {} {} {} {}".format(seed[0],seed[1], seed[2], seed[3], seed[4]))
+        print("seed: {} {} {} {}".format(seed[0],seed[1], seed[2], seed[3]))
 
         img = Image.new(mode="RGBA", size=vars.tar_res, color="white")
         img_draw = ImageDraw.Draw(img)
         img_font = ImageFont.truetype("special_imgs/arial.ttf", 12)
-        img_fp = ("./target_images/"+"{},{},{},{},{}".format(tar_shapes[seed[0]], tar_colors_names[seed[1]], seed[2], seed[3],tar_colors_names[seed[4]]))+".png"
+        img_fp = ("./target_images/"+"{},{},{},{}".format(tar_shapes[seed[0]], tar_colors_names[seed[1]], seed[2],tar_colors_names[seed[3]]))+".png"
         # print(tar_colors[seed[1]])
         # print(ImageColor.getcolor([0,0,255],"RGBA"))
 
@@ -66,10 +66,13 @@ def gen_targets(amount):
         if(seed[0] <= 5):
             img_draw.regular_polygon([img.size[0]/2,img.size[1]/2,img.size[0]/2], seed[0]+3, fill=tar_colors_names[seed[1]])
 
-        im = img.rotate(seed[3], fillcolor=(0,0,0,0))
-        im_draw = ImageDraw.Draw(im)
+        # img.show()
+        # im = img.rotate(seed[3], fillcolor=(0,0,0,0),expand=True)
+        im_draw = ImageDraw.Draw(img)
         # print("pog")
-        im_draw.text([vars.tar_res[0]/2,vars.tar_res[1]/2],seed[2], fill=tar_colors_names[seed[4]] , anchor="mm", font=img_font)
-        # im.show()
-        im.save(img_fp,format="PNG")
+        im_draw.text([vars.tar_res[0]/2,vars.tar_res[1]/2],seed[2], fill=tar_colors_names[seed[3]] , anchor="mm", font=img_font)
+        # img.show()
+        img.save(img_fp,format="PNG")
     return
+
+gen_targets(10)
