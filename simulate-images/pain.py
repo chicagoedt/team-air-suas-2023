6bedd3dcac6b41d3ce91f58428b434b5cdd8ecbe
Asik007 <yugioh705@gmail.com>
2023-01-03 21:57:41 -0600

    All I have done testenv has like what the script really should be.

--- simulate-images/pain.py
+++ simulate-images/pain.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageOps
 import math
+from time import sleep
 import numpy as np
 import random
 from PIL import ImageTransform
@@ -13,47 +14,50 @@ import os
 
 def gen_valid_cam():
     bgd_img = Image.open("master_background.png")
-    # global intersection
-    # global cam
-    # global bgd
-    runway = Polygon(([2932, 2688], [2864, 3500], [6800, 3600], [6850, 2800]))
-    bgd_w = bgd_img.width
-    bgd_h = bgd_img.height
-    bgd = Polygon(([0, 0], [bgd_h, 0], [bgd_h, bgd_w], [0, bgd_w]))
+    runway = Polygon(([2932, -2688], [2864, -3500], [6800, -3600], [6850, -2800]))
+    bgd = Polygon(([0, 0], [bgd_img.width, 0], [bgd_img.width, -1 * bgd_img.height], [0, -1 * bgd_img.height]))
     valid = False
-    fnl_box = []
-    corner = []
-    rot_arr = []
-    rot_tht = 0
+    # print(valid)
+    # global box, intersection, rot_tht
     while valid == False:
-        rot = random.randint(0, 359)
-        # rot = 0
+        # rot = random.randint(0, 359)
+        rot = 0
         rot_tht = math.radians(rot)
-        print(rot)
-        corner = [random.randint(0, bgd_w - 1), random.randint(0, bgd_h - 1)]
-        box = [[0, 0], [4032, 0], [4032, 3040], [0, 3040]]
-        rot_arr = [[math.cos(rot_tht), -1 * math.sin(rot_tht)], [math.sin(rot_tht), math.cos(rot_tht)]]
-        rot_box = np.dot(box, rot_arr)
-        fnl_box = []
-        for i in range(len(box)):
-            coord = np.add(rot_box[i], corner)
-            if 0 < coord[0] < bgd_img.width - 1 and 0 < coord[1] < bgd_img.height - 1:
-                valid = True
-            else:
-                valid = False
-                break
-            fnl_box.append(list(coord))
-        if len(fnl_box) == 4:
-            cam = Polygon(fnl_box)
-            if not (cam.intersects(runway)):
+        # print(rot)
+        # print(rot)
+        corner = [random.randint(0, bgd_img.width - 1), random.randint(-1*(bgd_img.height - 1),0)]
+        print(corner)
+        box = Polygon([[0, 0], [4032, 0], [4032, -3040], [0, -3040]])
+        # print(box)
+        # plt.plot(*box.exterior.xy, label="pre", color="yellow")
+
+        box = affinity.rotate(box,rot,(0,0))
+        # plt.plot(*box.exterior.xy, label = "rot", color="orange")
+
+        # print(box)
+        box = affinity.translate(box,xoff=corner[0],yoff=corner[1])
+        # print(valid)
+        print(box)
+        plt.plot(*box.exterior.xy, label = "final", color="Red")
+
+        # plt.show()
+        # sleep(10)
+        if box.within(bgd):
+            # print("enter looop")
+            if not (box.intersects(runway)):
                 valid = False
             else:
                 print("pog")
-                intersection = cam.intersection(runway)
-                print(cam.exterior.coords)
-                print(intersection.exterior.coords)
-                return cam, intersection, bgd, rot
-    return
+                intersection = box.intersection(runway)
+                print(*box.exterior.coords)
+                print(*intersection.exterior.coords)
+                valid = True
+                plt.plot(*intersection.exterior.xy)
+                plt.show()
+                return box, intersection, bgd, rot
+
+
+
 
 
 
@@ -65,8 +69,8 @@ def gen_valid_tar(background,target):
     rot_arr = []
     rot_tht = 0
     while valid == False:
-        rot = random.randint(0, 359)
-        # rot = 0
+        # rot = random.randint(0, 359)
+        rot = 0
         print(rot)
         corner = [random.randint(int(minx),int(maxx)),random.randint(int(miny),int(maxy))]
         box = Polygon([[0, 0], [target.width, 0], [target.width, target.height], [0, target.height]])
