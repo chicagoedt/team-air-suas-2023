4f2be00bd6723b955f192d43e954a993c8fb5965
Asik007 <yugioh705@gmail.com>
2023-01-06 17:04:52 -0600

    finally finished ( to use import pain and run fnl_valid_gen)

--- simulate-images/pain.py
+++ simulate-images/pain.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw, ImageOps
 import math
 from time import sleep
+import vars
 import numpy as np
 import random
 from PIL import ImageTransform
@@ -19,22 +20,10 @@ def gen_valid_cam(bgd_img):
         (6754, -3670),
         (6786, -2824)
     ))
-    runway_flip = affinity.scale(runway,yfact=-1,origin=(0,0))
-    # runway_flip = affinity.scale(runway,yfact=,xfact=.95,origin="centroid")
-    # bgd_img_draw = ImageDraw.Draw(bgd_img)
-    # print(*runway_flip.exterior.coords)
-    # bgd_img_draw.polygon(runway_flip.exterior.coords,fill="black")
-    # bgd_img.show()
     bgd = Polygon(([0, 0], [bgd_img.width, 0], [bgd_img.width, -1 * bgd_img.height], [0, -1 * bgd_img.height]))
     valid = False
-    # print(valid)
-    # global box, intersection, rot_tht
     while valid == False:
         rot = random.randint(0, 359)
-        # rot = 0
-        rot_tht = math.radians(rot)
-        # print(rot)
-        # print(rot)
         corner = [random.randint(0, bgd_img.width - 1), random.randint(-1*(bgd_img.height - 1),0)]
         # print(corner)
         box = Polygon([[0, 0], [4032, 0], [4032, -3040], [0, -3040]])
@@ -63,80 +52,88 @@ def gen_valid_cam(bgd_img):
                 # print(*intersection.exterior.coords)
                 valid = True
                 plt.plot(*intersection.exterior.xy)
-                plt.show()
-                return box, intersection, bgd, rot
+                # plt.show()
+
+                inter_trans = affinity.translate(intersection, xoff=-1 * corner[0],yoff=-1 * corner[1])
+                inter_rot1 = affinity.rotate(inter_trans, -1 * rot, origin=(0, 0))
+                inter_rot = affinity.rotate(inter_rot1, 180, origin=(vars.cam_res[0]//2,vars.cam_res[1]//-2,))
+                inter_fnl = affinity.scale(inter_rot, yfact=-1, origin=(0, 0))
+                inter_transformed = affinity.scale(inter_fnl, xfact=-1, origin=(vars.cam_res[0]//2,vars.cam_res[1]//-2,))
+
+                cam_fnl_crop = affinity.scale(box, yfact=-1, origin=(0, 0))
+                cam_fnl_xy = np.ravel((cam_fnl_crop.exterior.coords[3] + cam_fnl_crop.exterior.coords[4] +
+                                       cam_fnl_crop.exterior.coords[1] + cam_fnl_crop.exterior.coords[2]))
+                cropped_img = bgd_img.transform((4032, 3040), ImageTransform.QuadTransform(cam_fnl_xy))
+
+                return box, intersection, inter_transformed, cropped_img
 
 
 
 
 
 
-def gen_valid_tar(background,target):
-    minx,miny,maxx,maxy = background.bounds
-    print(minx,miny,maxx,maxy)
+def gen_valid_tar(crop_img,runway_geom,target):
+    minx,miny,maxx,maxy = runway_geom.bounds
+    # print(minx,miny,maxx,maxy)
     valid = False
-    corner = []
-    rot_arr = []
-    rot_tht = 0
     while valid == False:
-        # rot = random.randint(0, 359)
-        rot = 0
+        rot = random.randint(0, 359)
+        # rot = 0
         print(rot)
         corner = [random.randint(int(minx),int(maxx)),random.randint(int(miny),int(maxy))]
         box = Polygon([[0, 0], [target.width, 0], [target.width, target.height], [0, target.height]])
 
-        dx = corner[0]
-        dy = corner[1]
+        # print(box)
+        plt.plot(*box.exterior.xy, label="pre", color="yellow")
 
-        box = affinity.rotate(box, rot, (0, 0))
-        box = affinity.translate(box,dx,dy)
+        box = affinity.rotate(box,rot,(0,0))
+        plt.plot(*box.exterior.xy, label = "rot", color="orange")
 
-        plt.plot(*box.exterior.xy)
+        # print(box)
+        box = affinity.translate(box,xoff=corner[0],yoff=corner[1])
+        # print(valid)
+        # print(box)
+        plt.plot(*box.exterior.xy, label = "final", color="Red")
+
         print(*box.exterior.coords)
-        if box.within(background):
+        if box.within(runway_geom):
+            # if box.within(runway_geom):
+            crop_img = crop_img.convert("RGBA")
+            target = target.convert("RGBA")
             valid = True
             print("pog")
-            return box,rot
+            tar_rot = target.rotate(rot,expand=True, fillcolor="#00000000")
+            tar_mnx, _miny, _maxx, tar_mxy = box.bounds
+            crop_img.alpha_composite(tar_rot, dest=(int(tar_mnx),int(tar_mxy)))
+            # crop_img.show()
+            box_real = affinity.scale(box,yfact=-1,origin=(0,tar_mxy))
+            return box,rot,crop_img,box_real
         else:
             valid = False
 
 
 
 
-bgd_img = Image.open("master_background.png")
-cam, intersection, bgd, rot = gen_valid_cam(bgd_img)
+def fnl_valid_img(amount):
+    for _ in range(amount):
+        print("Which image it is on rn:", _)
+        bgd_img = Image.open("master_background.png")
+        cam, intersection, bgd, cropped = gen_valid_cam(bgd_img)
 
-print(cam.exterior.coords[0])
-cam_trans = affinity.translate(cam,xoff=-1 * cam.exterior.coords[0][0], yoff=-1 * cam.exterior.coords[0][1])
-inter_trans= affinity.translate(intersection,xoff=-1 * cam.exterior.coords[0][0], yoff=-1 * cam.exterior.coords[0][1])
+        # plt.show()
+        bgd_img_drw = ImageDraw.Draw(cropped)
+        bgd_img_drw.polygon(bgd.exterior.coords,fill="Black")
+        # cropped.show()
 
-inter_rot1 = affinity.rotate(inter_trans,-1*rot,origin=(0,0))
-cam_rot = affinity.rotate(cam_trans,-1*rot,origin=(0,0))
-inter_rot = affinity.rotate(inter_rot1,180,origin=(cam_rot.centroid))
+        tar_files = os.listdir("target_images")
+        tar_path = random.choice(tar_files)
+        print(tar_path)
+        tar_img = Image.open("target_images/" + tar_path)
 
-cam_fnl = affinity.scale(cam_rot,yfact=-1, origin=(0,0))
-cam_fnl_crop = affinity.scale(cam,yfact=-1, origin=(0,0))
+        box, rot, crop_img, box_real = gen_valid_tar(cropped,bgd,tar_img)
 
-inter_fnl = affinity.scale(inter_rot,yfact=-1, origin=(0,0))
+        new_drw = ImageDraw.Draw(crop_img)
+        new_drw.polygon(box_real.exterior.coords,fill="white")
 
-inter_fnl = affinity.scale(inter_fnl,xfact=-1, origin=(cam_fnl.centroid))
-
-bgd = affinity.scale(bgd,yfact=-1, origin=(0,0))
-plt.plot(*cam.exterior.xy)
-plt.plot(*intersection.exterior.xy)
-plt.plot(*inter_rot1.exterior.xy)
-plt.plot(*cam_rot.exterior.xy)
-
-plt.plot(*inter_fnl.exterior.xy)
-plt.plot(*cam_fnl.exterior.xy)
-
-
-
-plt.show()
-cam_fnl_xy = np.ravel((cam_fnl_crop.exterior.coords[3]+cam_fnl_crop.exterior.coords[4]+cam_fnl_crop.exterior.coords[1]+cam_fnl_crop.exterior.coords[2]))
-print(*cam_fnl_crop.exterior.coords)
-
-test = bgd_img.transform((4032,3040),ImageTransform.QuadTransform(cam_fnl_xy))
-bgd_img_drw = ImageDraw.Draw(test)
-bgd_img_drw.polygon(inter_fnl.exterior.coords,fill="Black")
-test.show()
+        final_dir = "final_images/"
+        crop_img.save(final_dir+"{}_{}_{}_{}.png".format(box_real.exterior.coords))
