9287370de02aabc5ecc958613e9c9b0626a843a0
Christian Sanchez <91746384+ChristianSanchez-gse@users.noreply.github.com>
2023-02-05 11:11:12 -0600

    Added localization code

---
+++ object-detection/object-localization/main.py
@@ -0,0 +1,17 @@
+import cv2
+import ShapeDetector
+
+#for testing, ignore
+hist_h, hist_s, hist_v = ShapeDetector.getHSVHist("img.jpg")
+
+# Cv2 setup reading image
+img = cv2.imread("img.jpg")
+
+contourImg, coords, pointCount = ShapeDetector.findShape(img)
+print("\nThe coordinate location for the shape is {} and it has {} points.... final answer".format(coords, pointCount))
+cv2.imshow("contours", contourImg)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
