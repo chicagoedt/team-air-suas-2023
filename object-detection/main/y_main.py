6953b5cd11c99406e8b7f2157d24fb62b75761ee
Mightymanh <dphan8@uic.edu>
2023-02-11 13:16:49 -0600

    add main/: a folder where localization, color, shape, text detection is here. run y_main

---
+++ object-detection/main/y_main.py
@@ -0,0 +1,22 @@
+'''
+    
+'''
+
+# import sths
+from localization_crop import *
+from text_letterImageMapping import *
+
+ImagesByCamera_Folder = 'ImagesByCamera'
+CroppedImages_Folder = 'CroppedImages' 
+
+# input from judge
+inputsFromJudge = input('Enter letters given by judge, between each pair of letters is a space, like "A B C D F" not "AB CDF".\n'
+                        'You input: ')
+letters = inputsFromJudge.split()
+
+# object localization, crop
+cropFolder(ImagesByCamera_Folder, CroppedImages_Folder, 100, 100)
+print('------------------------------')
+
+# text detection, mapping letters to each images
+results = mapLettersToImages(letters, CroppedImages_Folder)
