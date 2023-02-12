d38384e6cb4cb2c9525246b5b8581c20c482f057
Mightymanh <dphan8@uic.edu>
2023-02-12 14:55:24 -0600

    decorate modules.py

--- object-detection/modules.py
+++ object-detection/modules.py
@@ -1,3 +1,6 @@
+'''
+    necessary modules for running y_main.py
+'''
 import sys
 import os
 
@@ -8,10 +11,10 @@ textDetection_path = os.path.join(currentDirectory, 'text-detection')
 
 # import crop from object-localization folder
 sys.path.append(objectLocalization_path)
-from crop import *
+from crop import *    # cropFolder()
 sys.path.pop()
 
 # import y_letterImageMapping from text-detection folder
 sys.path.append(textDetection_path)
-from y_letterImageMapping import *
+from y_letterImageMapping import *  # mapLettersToImages()
 sys.path.pop()
