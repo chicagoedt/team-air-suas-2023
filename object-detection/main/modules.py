90b6dad0b44ece166a411a779a96f391edfb63cb
Mightymanh <dphan8@uic.edu>
2023-02-12 14:22:11 -0600

    fix modules.py

--- object-detection/main/modules.py
+++ object-detection/main/modules.py
@@ -3,12 +3,12 @@ import sys
 objectLocalization_path = '/Users/mightymanh/Desktop/myCode/myPy/team-air-suas-2023/object-detection/object-localization'
 textDetection_path = '/Users/mightymanh/Desktop/myCode/myPy/team-air-suas-2023/object-detection/text-detection'
 
-# append crop from object-localization folder
+# import crop from object-localization folder
 sys.path.append(objectLocalization_path)
 from crop import *
 sys.path.pop()
 
-# append y_letterImageMapping from text-detection folder
+# import y_letterImageMapping from text-detection folder
 sys.path.append(textDetection_path)
 from y_letterImageMapping import *
 sys.path.pop()
