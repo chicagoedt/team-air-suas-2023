c26ae5fa61f127503c347937da851f71d3d514cc
Adam Beigel <31280510+abeige@users.noreply.github.com>
2023-02-21 18:58:35 -0600

    createTarget, drawShape, drawLetter

--- simulate-images/sim_image.py
+++ simulate-images/sim_image.py
@@ -1,15 +1,23 @@
 # imports
+import os
 from random import randint, choice
+
+from PIL import Image, ImageDraw, ImageFont
+from shapely import affinity
+from shapely.geometry import box
 
 
 # constants
-shapes = (
+SHAPES = (
     "triangle", "square", "pentagon", "hexagon", "heptagon",
     "octagon", "circle", "semicircle", "quartercircle",
     "rect", "star", "trap", "cross"
 )
+SPECIAL_CASES = ["semicircle", "quartercircle",
+                 "rect", "star", "trap", "cross"]
+POLYGONS = ["triangle", "square", "pentagon", "hexagon", "heptagon", "octagon"]
 
-colors = {
+COLORS = {
     "white": (255, 255, 255),  # White
     "black": (0, 0, 0),  # Black
     "gray": (127, 127, 127),  # Gray
@@ -25,7 +33,7 @@ colors = {
 
 # choose a color from colors, excluding if necessary
 def chooseColor(exclude=None):
-    validColors = list(colors.keys())
+    validColors = list(COLORS.keys())
     if exclude:
         validColors = [c for c in validColors if c != exclude]
 
@@ -49,7 +57,7 @@ def generateSeed(params=None) -> dict:
     if not seed["rotation"]:
         seed["rotation"] = randint(0, 359)
 
-    seed["shape"] = shapes[randint(0, 12)]
+    seed["shape"] = SHAPES[randint(0, 12)]
 
     if not seed["shapeColor"]:
         seed["shapeColor"] = chooseColor(exclude=seed.get("letterColor"))
@@ -63,19 +71,87 @@ def generateSeed(params=None) -> dict:
     return seed
 
 
+# draw shape on target
+def drawShape(img, shape, color):
+    draw = ImageDraw.Draw(img)
+
+    if (shape == "circle"):
+        draw.ellipse([0, 0, img.size], fill=COLORS[color], width=0)
+    elif (shape in SPECIAL_CASES):
+        with Image.open(vars.resourceDir + shape + ".bmp") as bitFile:
+            scaleFactor = vars.targetSize[0] / bitFile.width
+            newSize = [int(bitFile.width * scaleFactor),
+                       int(bitFile.height * scaleFactor)]
+            scaledBitFile = bitFile.resize(newSize)
+            draw.bitmap([0, 0], scaledBitFile, fill=COLORS[color])
+    else:
+        draw.regular_polygon(
+            [img.size[0] / 2, img.size[1] / 2, img.size[0] / 2],
+            POLYGONS.index(shape) + 3,
+            fill=color
+        )
+
+    return img
+
+
+# draw letter on target
+def drawLetter(img, letter, color):
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(
+        vars.resourceDir + "arial.ttf", int(vars.targetSize[0] / 2))
+
+    draw.text(
+        [vars.targetSize[0] / 2, vars.targetSize[1] / 2],
+        letter,
+        fill=COLORS[color],
+        anchor="mm",
+        font=font
+    )
+
+    return img
+
+
 class SimImage:
-    def __init__(self, s=None):
+    def __init__(self, s=None, f=None):
         self.snapshot = s
+        self.filename = f
         self.targets = []
 
     def createTarget(self, **kwargs):
         seed = generateSeed(kwargs)
 
-        target = None
+        # create a new image, draw shape and letter
+        target = Image.new(mode="RGBA", size=vars.targetSize, color="#0000")
+        target = drawShape(target, seed["shape"], seed["shapeColor"])
+        target = drawLetter(target, seed["letter"], seed["letterColor"])
 
-        polygon = None
+        # create a polygon for the target
+        polygon = box(0, 0, target.size[0], target.size[1])
 
-        self.targets.append((target, polygon, seed))
+        # rotate the target
+        target = target.rotate(
+            seed["rotation"],
+            expand=True,
+            fillcolor="#0000"
+        )
+
+        # rotate the polygon (negative for different origin)
+        polygon = affinity.rotate(polygon, -seed["rotation"])
+        polygon = affinity.translate(
+            polygon,
+            xoff=-polygon.bounds[0],
+            yoff=-polygon.bounds[1]
+        )  # snap to axes
+
+        self.targets.append({
+            "target": target,
+            "polygon": polygon,
+            "seed": seed,
+        })
+
+    def saveImage(self):
+        self.snapshot.convert("RGB") \
+            .save(os.path.join(vars.targetDir, self.filename + ".jpg"))
 
 
 if __name__ == "__main__":
